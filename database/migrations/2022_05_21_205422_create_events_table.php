b4f5aa965307fecb3847d9d9a45eaad7d7df8ba3
Matheus Melo Antiquera <matheus.melo.a@hotmail.com>
2022-05-21 18:56:27 -0300

    chore: :card_file_box: criar o migration da tabela events

---
+++ database/migrations/2022_05_21_205422_create_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger("type");
+            $table->unsignedBigInteger("origin");
+            $table->unsignedBigInteger("destination")->nullable();
+            $table->decimal('amount', 8, 4);
+            $table->foreign('origin')->references('id')->on('accounts');
+            $table->foreign('destination')->references('id')->on('accounts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};
